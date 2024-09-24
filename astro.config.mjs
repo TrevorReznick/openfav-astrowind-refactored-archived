bbf843b45c91cb31c884c6102c3c2c15d7ac7a06
Tuo Nome <double.facessss@gmail.com>
2024-09-24 23:11:52 +0200

    OF-42 fixing setting project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import openfav from './src/integrations/index'
 
@@ -9,6 +10,7 @@ import openfav from './src/integrations/index'
 export default defineConfig({
     site: 'https://example.com',
 	output: 'server',
+	adapter: vercel(),
     integrations: [
 		mdx(), 
 		sitemap(), 
