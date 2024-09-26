b4f76326b12f9bf9e152d6eaed20430446eb68c6
Tuo Nome <double.facessss@gmail.com>
2024-09-26 20:10:49 +0200

    OF-41 settings component and pages

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -3,25 +3,8 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Home',      
+      href: getPermalink('/homes/saas'),
     },
     {
       text: 'Pages',
@@ -57,68 +40,27 @@ export const headerData = {
       ],
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+      text: 'About',      
+      href: getPermalink('/about'),
     },
     {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      text: 'About',      
+      href: getPermalink('/about'),
     },
     {
-      text: 'Widgets',
-      href: '#',
-      target: '_blank'
+      text: 'Company',      
+      href: getPermalink('/homes/saas'),
     },
     {
       text: 'Production',
       href: 'https://bookmarks-list.netlify.app/',
     },
+    /*
+    {
+      text: 'Login',      
+      href: getPermalink('/login/signin'),
+    },
+    */    
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
---
+++ src/scripts/utils/permalinks-utils.ts
@@ -0,0 +1,64 @@
+import { SITE, APP_BLOG } from 'astrowind:config' // Importa le configurazioni per il sito e il blog
+import { trim } from '~/utils/utils'
+import slugify from 'limax' // Importa la libreria 'slugify' per trasformare stringhe in slug leggibili
+
+/**
+ * Rimuove eventuali barre '/' all'inizio e alla fine di una stringa.
+ * @param {string} s - La stringa da elaborare.
+ * @returns {string} - La stringa senza barre iniziali e finali.
+ */
+export const trimSlash = (s: string) => trim(trim(s, '/'));
+
+/**
+ * Combina i segmenti di percorso in una singola stringa di percorso.
+ * Rimuove barre iniziali e finali extra e aggiunge una barra finale se necessario.
+ * @param {...string} params - Segmenti di percorso da combinare.
+ * @returns {string} - Il percorso combinato.
+ */
+export const createPath = (...params: string[]) => {
+  //console.log('create path');
+  //console.log('params', params);
+
+  // Pulisce e combina i segmenti di percorso
+  const paths = params
+    .map((el) => trimSlash(el))
+    .filter((el) => !!el)
+    .join('/');
+
+  // Restituisce il percorso combinato con una barra finale se necessario
+  //console.log('/' + paths + (SITE.trailingSlash && paths ? '/' : ''));
+  return '/' + paths + (SITE.trailingSlash && paths ? '/' : '');
+};
+
+/**
+ * Pulisce e trasforma una stringa in uno slug valido.
+ * Rimuove spazi e caratteri speciali, e separa i segmenti con '/'.
+ * @param {string} text - La stringa da trasformare in slug.
+ * @returns {string} - La stringa trasformata in uno slug.
+ */
+export const cleanSlug = (text = '') =>
+    trimSlash(text)
+      .split('/')
+      .map((slug) => slugify(slug))
+      .join('/');
+  
+  // Crea gli slug base per blog, categorie e tag
+
+/**
+ * Crea un URL canonico basato sul percorso fornito.
+ * Aggiunge o rimuove una barra finale a seconda della configurazione del sito.
+ * @param {string} [path=''] - Il percorso da convertire in URL canonico.
+ * @returns {string | URL} - L'URL canonico.
+ */
+export const getCanonical = (path = ''): string | URL => {
+    //console.log('get canonical');
+    const url = String(new URL(path, SITE.site));
+  
+    // Aggiusta la barra finale dell'URL in base alla configurazione del sito
+    if (SITE.trailingSlash == false && path && url.endsWith('/')) {
+      return url.slice(0, -1);
+    } else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
+      return url + '/';
+    }
+    return url
+}
