e8e462af2a0725c69be606bf97a18f59a2271499
Tuo Nome <double.facessss@gmail.com>
2024-09-26 14:47:39 +0200

    OF-41 settings component and pages

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import type { HTMLAttributes} from 'astro/types'
 
+/* @@ Metadata @@ */
+
 export interface MetaData {
     title?: string
     ignoreTitleTemplate?: boolean  
@@ -33,4 +35,36 @@ export interface MetaDataTwitter {
     handle?: string;
     site?: string;
     cardType?: string;
+}
+
+/* @@ Widgets @@ */
+
+export interface Widget {
+    id?: string;
+    isDark?: boolean;
+    bg?: string;
+    classes?: Record<string, string | Record<string, string>>;
+}
+
+export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' | 'classes'> {
+    content?: string;
+    actions?: string | CallToAction[];
+    image?: string | unknown;
+}
+
+export interface Headline {
+    title?: string;
+    subtitle?: string;
+    tagline?: string;
+    classes?: Record<string, string>;
+}
+
+/* @@ Components @@ */
+
+export interface CallToAction extends Omit<HTMLAttributes<'a'>, 'slot'> {
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link';
+  text?: string;
+  icon?: string;
+  classes?: Record<string, string>;
+  type?: 'button' | 'submit' | 'reset';
 }
